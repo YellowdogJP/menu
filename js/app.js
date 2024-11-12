3521c4ddc7b415623379201080f464d7a0ec6701
YellowdogJP <joaopedroclarineta@gmail.com>
2024-11-12 16:34:17 -0400

    JS-AdicionarAoCarrinho-V1

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 })
 
 var cardapio = {};
+var MEU_CARRINHO = [];
 
 cardapio.eventos = {
 
@@ -61,16 +62,67 @@ cardapio.metodos = {
     },
 
     diminuirQuantidade: (id) => {
-        let qntdAtual = parseInt($('#qntd-carrinho-'+id).text())
+        let qntdAtual = parseInt($('#qntd-carrinho-' + id).text())
         if (qntdAtual > 0) {
             $('#qntd-carrinho-' + id).text(qntdAtual - 1)
         }
     },
 
     aumentarQuantidade: (id) => {
-        let qntdAtual = parseInt($('#qntd-carrinho-'+id).text())
+        let qntdAtual = parseInt($('#qntd-carrinho-' + id).text())
         $('#qntd-carrinho-' + id).text(qntdAtual + 1)
     },
+
+    adicionarAoCarrinho: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+
+        if (qntdAtual > 0) {
+
+            //obter a categoria ativ
+            var categoria = $(".container-menu").attr('id').split('menu-')[1];
+            ///obtem a lista de itens
+            let filtro = MENU[categoria];
+            //obtem o item
+            let item = $.grep(filtro, (e, i) => { return e.id == id });
+
+
+            if (item.lenght > 0) {
+                //validar se já existe item no carrinho
+                let existe = $.grep(MEU_CARRINHO, (elem, index) => { return elem.id == id });
+                //caso já exista o item no carrinho, só altera a quantidade
+                if(existe.lenght >0){
+                    let objIndex = MEU_CARRINHO.findIndex((obj => obj.id ==id));
+                    MEU_CARRINHO[objIndex].qntd = MEU_CARRINHO[objIndex].qntd + qntdAtual;
+                }
+                //caso não exista o item no carrinho, adiciona ele
+                else{
+                    item[0].qntd= qntdAtual;
+                    MEU_CARRINHO.push(item[0])
+                }
+            }
+
+        }
+    },
+
+    //atualiza o badge de totais dos botões "Meu carrinho"
+    atualizarBadgeTotal:() =>{
+        var total = 0;
+
+        $.each(MEU_CARRINHO, (i,e) => {
+            total += e.qntd
+        })
+        if (total > 0){
+            $("botao-carrinho").removeClass('hidden')
+            $(".container-total-carrinho").addClass('hidden');
+        }
+        else{
+            $(".botao-carrinho").addClass('hidden')
+            $(".container-total-carrinho").html(total);
+        }
+        $(".badge-total-carrinho").html(total);
+    }
+
 }
 
 cardapio.templates = {
@@ -91,7 +143,7 @@ cardapio.templates = {
             <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
             <span class="btn-numero-itens" id="qntd-carrinho-\${id}">0</span>
             <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
-            <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
+            <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="fa fa-shopping-bag"></i></span>
         </div>
         </div>
     </div>`
